35058053348a589ebdd5092928879754ef666378
MrBiscuitss <157910494+MrBiscuitss@users.noreply.github.com>
2025-11-02 22:59:04 -0500

    Implement Liquid shortcode for image rendering
    Added a Liquid shortcode for rendering images with captions.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,20 @@
+module.exports = function (eleventyConfig) {
 
-// The export statement makes these settings available to other files in 11ty
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addLiquidShortcode("picture", function (src, alt) {
+    return `<figure class="photo">
+              <img src="/${src}" alt="${alt}">
+              <figcaption>${alt}</figcaption>
+            </figure>`;
+  });
+
   eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addPassthroughCopy("images");
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
 };
